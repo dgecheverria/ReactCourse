4d76a5c564ce6b61fd62081852e5c603e457a5bb
dgecheverria <diego@northstreetcreative.com>
2025-04-15 23:03:56 -0500

    First commit Course React 2025

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+const nombre = 'Diego';
+const apellido =  'Root';  
+
+
+let valorDado = 5 ;
+valorDado = 4 ;
+
+console.log(nombre, apellido, valorDado); 
+
+//var No se debe usar...
+
+if(true){
+    const nombre = 'Peter';
+
+    console.log(nombre);
+}
+
+console.log(valorDado);
